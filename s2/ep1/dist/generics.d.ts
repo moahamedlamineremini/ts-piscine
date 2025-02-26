f7897bfcc2133e3f0564df9f47eae47ebac1b824
moahamedlamineremini <124696525+moahamedlamineremini@users.noreply.github.com>
2025-02-26 13:57:35 +0100

    s2 ep1 fini

---
+++ s2/ep1/dist/generics.d.ts
@@ -0,0 +1,6 @@
+declare function premierelement<T>(tableau: T[]): T;
+declare let tableau1: number[];
+declare let tableau2: string[];
+declare let tableau3: {
+    nom: string;
+}[];
---
+++ s2/ep1/src/generics.ts
@@ -0,0 +1,12 @@
+function premierelement<T>(tableau: T[]): T 
+{
+    return tableau[0];
+}
+
+let tableau1 = [1, 2, 3];
+let tableau2 = ["a", "b", "c"]; 
+let tableau3 = [{ nom: "Alice" }, { nom: "Bob" }];
+
+console.log(premierelement(tableau1)); // 1
+console.log(premierelement(tableau2)); // a
+console.log(premierelement(tableau3)); // { nom: "Alice" }
