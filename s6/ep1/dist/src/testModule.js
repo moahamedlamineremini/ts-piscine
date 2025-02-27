036fbb699390b64cf76807cb996051cdf9705eb3
moahamedlamineremini <124696525+moahamedlamineremini@users.noreply.github.com>
2025-02-27 12:04:41 +0100

    s6 ep1 fini

---
+++ s6/ep1/dist/src/testModule.js
@@ -0,0 +1,3 @@
+import { alerter, logger } from "../mon-module/index.js";
+alerter("Test alerte");
+logger("Test log");
---
+++ s6/ep1/src/testModule.ts
@@ -0,0 +1,4 @@
+import { alerter, logger } from "../mon-module/index";
+alerter("Test alerte");
+
+logger("Test log");
